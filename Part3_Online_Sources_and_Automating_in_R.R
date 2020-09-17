bc6b1d6dfb119323f16d1e8f2868aeaf11e14736
Lisanne <lisanne.petracca@yahoo.com>
2020-09-16 23:43:36 -0400

    Added a couple time series resources from Data Carpentry workshop

---
+++ Part3_Online_Sources_and_Automating_in_R.R
@@ -0,0 +1,5 @@
+#### AMANDAS CODE
+
+#resources LP came across, ditch em or not
+#import .tifs in single step and plot time series in ggplot: https://datacarpentry.org/r-raster-vector-geospatial/12-time-series-raster/index.html
+#time series aesthetics in ggplot: https://datacarpentry.org/r-raster-vector-geospatial/13-plot-time-series-rasters-in-r/index.html

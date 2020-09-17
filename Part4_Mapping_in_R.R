1f0266a36fbbe7e4c929cbd289197da303a6daa7
Lisanne <lisanne.petracca@yahoo.com>
2020-09-17 00:02:41 -0400

    Resources for mapping.

--- Part4_Mapping_in_R.R
+++ Part4_Mapping_in_R.R
@@ -1,2 +1,5 @@
 #for LP:
-#for rasters: https://datacarpentry.org/r-raster-vector-geospatial/02-raster-plot/index.html
+#for rasters (ggplot) : https://datacarpentry.org/r-raster-vector-geospatial/02-raster-plot/index.html
+#for vector (ggplot) : https://datacarpentry.org/r-raster-vector-geospatial/07-vector-shapefile-attributes-in-r/index.html
+#                      https://datacarpentry.org/r-raster-vector-geospatial/08-vector-plot-shapefiles-custom-legend/index.html
+#for times series (ggplot): https://datacarpentry.org/r-raster-vector-geospatial/13-plot-time-series-rasters-in-r/index.html

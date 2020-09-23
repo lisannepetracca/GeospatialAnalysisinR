6640bcd4c78b793d97d97962fd577858532f7a0e
Lisanne <lisanne.petracca@yahoo.com>
2020-09-23 15:06:45 -0400

    Pre-workshop code

---
+++ PreWorkshop_Steps.R
@@ -0,0 +1,41 @@
+#Let's run through some steps to make sure we are good to go for Friday's workshop
+#First, let's install some packages
+
+#We will install "devtools" first to get two packages that are not in the CRAN library
+#***PLEASE HIT ENTER WHEN IT ASKS WHAT UPDATES YOU WANT FOR THE SECOND PACKAGE***
+install.packages("devtools")
+devtools::install_version("velox", version = "0.2.0")
+devtools::install_github("ropensci/FedData")
+
+#then we will install other packages that *are* in the CRAN library
+#***IF IT ASKS TO RESTART R, ACCEPT THIS STEP***
+install.packages(c("sp", "sf", "raster", "rgeos", "ggplot2", "dplyr", "units", "rnaturalearth", "rgdal", "ggmap",
+                   "move","adehabitatHR","rgbif", "grid"))
+#***RED TEXT DOES NOT MEAN IT DIDN'T WORK. THERE IS ONLY AN ISSUE IF YOU SEE SOMETHING LIKE "Error in install.packages>"***
+
+#now let's get these packages loaded into R
+library(sp)
+library(sf)
+library(raster)
+library(rgeos)
+library(ggplot2)
+library(dplyr)
+library(units)
+library(rnaturalearth)
+library(rgdal)
+library(velox)
+library(ggmap)
+library(move)
+library(adehabitatHR)
+library(rgbif)
+library(grid)
+
+#now we will set our working directory
+setwd("C:\\Users\\lspetrac\\Desktop\\Geospatial_Analysis_in_R") 
+      #CHANGE DIRECTORY TO WHERE YOUR "Geospatial_Analysis_in_R" FOLDER IS
+
+#and then read in two types of geospatial data
+elev <- raster("aster_image_20160624.tif") 
+honduras_boundary <- st_read("Honduras_Border.shp")
+
+#WHOOO HOOO! WE'RE DONE!

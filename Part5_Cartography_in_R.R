8d0b4ca1c2868056e29b89e5b4b394290ec54290
Lisanne <lisanne.petracca@yahoo.com>
2020-09-24 21:31:36 -0400

    clean-up edits to part 5

--- Part5_Cartography_in_R.R
+++ Part5_Cartography_in_R.R
@@ -1,26 +1,24 @@
-#for times series (ggplot): prob more appropriate for AC section
-#https://datacarpentry.org/r-raster-vector-geospatial/13-plot-time-series-rasters-in-r/index.html
+#let's set our working directory first
+setwd("C:\\Users\\lspetrac\\Desktop\\Geospatial_Analysis_in_R")
+
+#and let's load all the libraries we need
+library(sf)
+library(ggplot2)
+library(dplyr)
+library(raster)
+library(ggmap)
 
 # ---- LET'S HAVE SOME FUN WITH MAPPING! ----
 
 # ---- VECTOR ONLY ----
 
-
 #we will return to our Zimbabwe data for the vector mapping
-library(sf)
-library(ggplot2)
-library(dplyr)
-library(raster)
-library(grid)
-
-setwd("G:/My Drive/GitHub/GeospatialAnalysisinR/Data/Example_Zimbabwe")
-
 #first, let's read in our shapefile of Hwange NP (polygon)
-HwangeNP <- st_read("Hwange_NP.shp")
+HwangeNP <- st_read("Example_Zimbabwe\\Hwange_NP.shp")
 #then our roads (line)
-roads <- st_read("ZWE_roads.shp")
+roads <- st_read("Example_Zimbabwe\\ZWE_roads.shp")
 #then our waterholes (point)
-waterholes <- st_read("waterholes.shp")
+waterholes <- st_read("Example_Zimbabwe\\waterholes.shp")
 
 #let's project roads to wGS 1984 UTM Zone 35S to match the others
 roads <- st_transform(roads, crs = 32735)
@@ -71,12 +69,11 @@ ggplot() +
   scale_color_manual(values=waterhole_colors)+
   labs(color = 'Waterhole type')+
   ggtitle("Waterhole Types in Hwange NP")+
-  guides(colour = guide_legend(title.hjust = 1.5))+
-  theme(plot.title = element_text(size=20),
-        legend.position="bottom",
-        legend.title=element_text(size=16),
-        legend.text = element_text(size = 16), 
-        legend.box.background = element_rect(size = 1)) + 
+  theme(plot.title = element_text(size=20), #this changes size of plot title
+        legend.position="bottom", #changes legend position
+        legend.title=element_text(size=16), #changes size of legend title
+        legend.text = element_text(size = 16), #changes size of element text in legend
+        legend.box.background = element_rect(size = 1)) + #adds a legend box of width 1
   coord_sf()
 
 #ok, so that's great for plotting a single shapefile
@@ -116,30 +113,39 @@ ggplot() +
 
 #And, for a final foray with vector mapping, let's explore different basemaps
 #be sure that you have a 64-bit version of java installed if you are using 64-bit R
-library(ggmap)
 #first, you need to register for an API key here:  https://cloud.google.com/maps-platform/
 #then you need to type in your key in order to enable Google's map services
-register_google(key = "AIzaSyB6hLwmyh0NAURYfeLQ9j7ivOgOk5ip43o", write = TRUE)
+register_google(key = "TYPE KEY HERE", write = TRUE)
+
+#once you have registered, you should save your key to a .csv in an easily-accessible place
+#use a single column named "Key" and paste the key underneath
+key<-read.csv("C:\\Users\\lspetrac\\Dropbox\\google_api.csv")
+register_google(key = key$Key)
 
 #let's get the bounding box of Zimbabwe
 #country boundaries from https://gadm.org/
+#yet another means of getting country boundaries
 Zimbabwe <- getData("GADM",country="Zimbabwe",level=0)
+#convert to sf object
 Zimbabwe_sf <- st_as_sf(Zimbabwe)
-plot(Zimbabwe_sf)
+#plot it
+plot(Zimbabwe_sf[1])
+#get the extent
 extent <- st_bbox(Zimbabwe_sf)
 
-#let's do a "watercolor" theme from "stamen" source
-#let's get Hwange NP in there
+#let's do "watercolor" and "terrain" themes from "stamen" source
 
-#let's provide the coordinates for the extent, as well as the zoom
+#we need to provide the coordinates for the extent, as well as the zoom
 #lower numbers means coarser zoom; can go from 3-21
+#these lines will get the basemap tiles
 stamen_watercolor <- get_stamenmap(bbox = c(extent[[1]], extent[[2]], extent[[3]], extent[[4]]),
                                    maptype = "watercolor", 
                                    crop = T, zoom=8)
 stamen_terrain <- get_stamenmap(bbox = c(extent[[1]], extent[[2]], extent[[3]], extent[[4]]),
                                 maptype = "terrain", 
                                 crop = T, zoom=7)
-#getting a googlemap is a bit different because it works on a center coordinate
+#now we will extract files from google "satellite" and "hybrid" basemaps
+#getting google tiles is a bit different because they work on a center coordinate
 google_satellite <- get_googlemap(center = c(29.5, -19),
                                   maptype = "satellite", 
                                   crop = F, zoom=6)
@@ -166,18 +172,19 @@ ggmap(google_hybrid) +
   coord_sf(xlim=c(25,34), ylim=c(-23,-15))+
   ggtitle("Source = 'google', type= 'hybrid'")
 
-# ---- RASTER ONLY ----
+#### ---- INCLUDING RASTER DATA ----####
 
 #let's return to plotting elevation in Hwange NP
 #let's read in that cropped elevation file we already made
 
-elev <- raster("elev_Hwange.tif")
+elev <- raster("Example_Zimbabwe\\elev_Hwange.tif")
 
 #and let's see it quickly using the plot function in raster
 plot(elev)
 
 #to plot a raster in ggplot, remember that we need to convert it to a data frame first
 elev_df <- as.data.frame(elev, xy=TRUE)
+#see what the data frame looks like
 head(elev_df)
 
 #now we can get started
@@ -195,6 +202,7 @@ elev_df_fourgroups %>%
   count()
 
 #man, we have NA values. where are they? 
+#we can use "na.value = "color"" to show where those pixels are
 ggplot() +
   geom_raster(data = elev_df_fourgroups , aes(x = x, y = y, fill = elev_Hwange)) +
   scale_fill_viridis_c(na.value = 'red') 
@@ -203,7 +211,7 @@ ggplot() +
 #can use trim() argument in raster package to get rid of these cells, but we're ok for now
 #trim gets rid of NAs in the outer rows and columns 
 
-#one trick of getting around NAs in ggplot2 is removing those rows where the raster value is NA
+#one trick of getting around NAs is removing those rows where the raster value is NA
 #otherwise NA will show up in the legend & this is annoying
 elev_df_fourgroups_noNA <- elev_df_fourgroups[!is.na(elev_df_fourgroups$elev_Hwange), ]
 
@@ -281,79 +289,4 @@ ggplot() +
   scale_color_manual(values = waterhole_colors, name = "Waterhole type") +
   theme(axis.title = element_blank())+
   coord_sf()
-
-
-
-
-########### MAKING MULTIPLOTS
-
-
-sample_vp_1 <- viewport(x = 0, y = 0, 
-                        width =0.5, height = 1,
-                        just = c("left", "bottom"))
-sample_vp_2 <- viewport(x = 0.5, y = 0, 
-                        width = 0.5, height = 1,
-                        just = c("left", "bottom"))
-sample_vp_3 <- viewport(x = 0, y = .8, 
-                        width = 1, height = .1,
-                        just = c("left", "bottom"))
-
-dev.off()
-pushViewport(sample_vp_1)
-grid.draw(rectGrob(gp = gpar(col = "green",lwd=4)))
-popViewport(1)
-
-pushViewport(sample_vp_2)
-grid.draw(rectGrob(gp = gpar(col = "blue",lwd=4)))
-popViewport(1)
-
-pushViewport(sample_vp_3)
-grid.draw(rectGrob(gp = gpar(col = "black",lwd=4)))
-popViewport(1)
-
-
-map<-ggmap(stamen_watercolor) +
-  geom_sf(data = Zimbabwe_sf, color = "black", fill = NA, size=2, inherit.aes = FALSE) +
-  geom_sf(data = HwangeNP, color = "darkgreen", fill = NA, size=1, inherit.aes = FALSE)
-map2<-ggmap(google_satellite) +
-  geom_sf(data = Zimbabwe_sf, color = "black", fill = NA, size=2, inherit.aes = FALSE) +
-  geom_sf(data = HwangeNP, color = "white", fill = NA, size=1, inherit.aes = FALSE) +
-  coord_sf(xlim=c(25,34), ylim=c(-23,-15))
-
-
-dev.off()
-pushViewport(sample_vp_1)
-grid.draw(ggplotGrob(map))
-popViewport(1)
-
-
-pushViewport(sample_vp_2)
-grid.draw(ggplotGrob(map2))
-popViewport(1)
-
-pushViewport(sample_vp_3)
-grid.text("Just looking at some maps")
-popViewport(1)
-
-
-
-w<-ggplot() +
-  geom_raster(data = elev_df, aes(x = x, y = y, fill=elev_Hwange)) +
-  geom_sf(data = HwangeNP, color = "black", fill = NA, size=2) +
-  geom_sf(data=waterholes, aes(color=factor(TYPE)), size=3)+
-  scale_fill_viridis_c(name = "Elevation (m)")+
-  scale_color_manual(values = waterhole_colors, name = "Waterhole type") +
-  theme(axis.title = element_blank())+
-  coord_sf()
-
-dev.off()
-grid.draw(ggplotGrob(w))
-md_inset <- viewport(x = 0, y = 0, 
-                     just = c("left", "bottom"),
-                     width = 0.3, height = 0.32)
-pushViewport(md_inset)
-grid.draw(rectGrob(gp = gpar(alpha = 0.5, col = "white")))
-grid.draw(ggplotGrob(map2))
-grid.draw(rectGrob(gp = gpar(fill = NA, size=2,col = "black")))
-popViewport()
 

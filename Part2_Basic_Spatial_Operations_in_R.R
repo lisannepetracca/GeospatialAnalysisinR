567b556fa951a702ed0cb258506e9a0228911632
Cheeseman <Amanda.Cheeseman@sdstate.edu>
2023-06-29 11:08:43 -0500

    Update the code from sp/sf/raster to terra/sf

--- Part2_Basic_Spatial_Operations_in_R.R
+++ Part2_Basic_Spatial_Operations_in_R.R
@@ -1,157 +1,128 @@
-#install.packages(c("sp", "sf", "raster"))
-library(sp)
-library(sf)
-library(raster)
-setwd("C:/Users/acheesem/Desktop/ESF/Classes Taught/GIS in R workshop")#Change to your working directory path
-
-##part 1
-#Make an object called obj with 1 element 'x'
-Obj<-"x"
-
-#run obj it will return 'x'
-Obj
-
-#Make a vector (NOTE this is not the same as a spatial vector data type)
-lis<-c(1,5,7)
-
-#inspect 'lis'
-lis
-
-#select the second item in the vector lis
-lis[2]
-
-#select items 2 through 3 in lis
-lis[c(2:3)]
-
-#select items 1 and 3 in the list
-lis[c(1,3)]
-
-#remove item 2 from lis, store in new object called 'lis2'
-lis2<-lis[-2]
-
-lis2# look at lis2
-
-#replace item 2 in lis with the number 25
-lis[2]<-25
-lis#look at lis
-
-#make a data frame called df
-df<-data.frame(letters=c("a","b","c"),numbers=lis)
-
-#inspect df
-df
-
-#inspect data
-head(df) #look at first few rows
-tail(df)#look at last few rows
-str(df)#look at data structure
-summary(df)#look at data summaries
-
-#look at numbers column
-df$numbers
-
-#look at numbers column using index
-df[,2]
-
-#look at row 3 in numbers column
-df[3,2]
-
-#Run calculations or operations on data
-#Take the mean of the numbers column
-mean(df$numbers)
-
+#install.packages(c("terra"))
+library(terra)
+setwd("D:/My Drive/Synced Desktop/SDSU/Workshops Taught/GeospatialAnalysisinR_4hrs_2023/Geospatial_Analysis_in_R")#Change to your working directory path
 
 ###CAN WORK WITH SPATIAL DATA AS DATA FRAME 
 ##create unprojected spatial data 
-  #NO PROJECTION - NOT GOOD
-data<-data.frame(long=c(-76.13332,-76.86515,-76.851651),
+#NO PROJECTION - NOT GOOD
+data<-data.frame(long=c(-76.13332,-76.86515,-76.851651), # c() concatenates values separated by commas 
                  lat=c(42.85632,42.65465,42.51311))
+data#Inspect to see what data looks like
 
 #plot spatial data
 plot(data)
+#note if "Error in plot.new() : figure margins too large" resize the plot window 
+#in the (default lower right window) to be larger
+
 
 #########################################################################
-##### SPATIAL DATA TYPES SP, SF AND RASTER
+##### SPATIAL DATA TYPES in terra
 
-##Create projected spatial data with sp
+##Create projected spatial data from data.frame with terra
 #define coordinate system using EPSG code
-crdref <- crs("+init=epsg:4326")
-#inspect the CRS
-crdref
+crdref <- "EPSG:4326"
 
-#create spatial points class object names pts from data
-data#remember what data looks like?
-pts <- SpatialPoints(cbind(data$long,data$lat), proj4string=crdref)
+#create SpatVector class object names pts from data
+?vect# use the ? before a function to see which arguments are needed and their format!! 
+pts <-  vect(data,geom = c("long", "lat"), crs = crdref)
 
 #inspect pts
 pts
 plot(pts)
 
-##Create spatialpointsdataframe
+##Create an spatVector object with data
 #Create attributes corresponding to the row from data 
-  #(alternitively you would pull from your database/csv etc.)
-  # here creating sites pond, river, and forest, and ID for each row in data
+#(alternatively you would pull from your database/csv etc.)
+# here creating sites pond, river, and forest, and ID for each row in data
 att<-data.frame(site=c("Pond","River","Forest"),ID=1:nrow(data))
+
 #look at att
 att
 
-#use SpatialPOintsDataFrame() function to add attributes to points
-spdf<-SpatialPointsDataFrame(pts,data=att,proj4string = crdref)
+#use st_as_sf() function to add attributes to points
+#how do we do this?
+#really great for determining if sp or sf objects are required
+sf.df<-cbind(pts,att)
 
+#look at sf.df
+sf.df
 
-#look at spdf
-spdf
-#plot(spdf)
+#write st.df to a shapefile using function write.sf() 
+writeVector(sf.df,"myshapefile.shp",overwrite=T)
 
-#write spdf to a shapefile using function shapefile () in the raster package
-shapefile(spdf,"myshapefile.shp",overwrite=T)
+#read in myshapefile using the read.sf() function 
+shp<-vect("myshapefile.shp")
 
-#read in myshapefile using the shapefile() function in the raster package
-shp<-shapefile("myshapefile.shp")
+#Inspect and check loaded simple features object created from the shapefile
+shp#look shp
+str(shp)#look at data
 
-#Inspect and check loaded shapefile
-class(shp)#look at class
-head(shp)#look at data
-str(shp)#look at structure
 crs(shp)#look at coordinate reference system - note can be saved to object & applied to other datasets
-plot(shp)#plot shapefile
+#wow thats a lot -how about we try this
+crs(shp,describe=T)
+#much better!!
 
+#lets plot it
+plot(shp,col=as.factor(shp$site))#where 1 specifies the data column number to plot
 
-##Convert the shp to data frame
+##Convert the sf object to data frame
 geo_data<-data.frame(shp)
 
 #look at geo_data
 geo_data
 
-### create spatial class using sf package and geo_data
-sf.pts<-st_as_sf(geo_data, coords = c("coords.x1", "coords.x2"), crs = crs(shp))
+#aww no coordinates -what if we want them??
+#get coordinates
+coords<-data.frame(crds(shp))
 
-#inspect
-sf.pts
+#and combine/view
+(geo_dat_coords<-cbind(geo_data,coords))
 
+###What about the other vector packages 
+#sp is out of date but sf still good!
 
-#alternatively we could convert directly from sp object
-st_as_sf(shp)
+#convert sp to spatVector object using vect()
 
-#read .shp with as sf using st_read() function
-nc <- st_read("myshapefile.shp")
+#convert from spatVector to simplefeatures using st_as_sf()
+library(sf)#load the sf library
 
-#write  sf to .shp with st_write() function
-st_write(nc, "myshapefile.shp", append=F)
+#Check your spatVector object
+shp
 
+#and convert & inspect!
+(simple_features<-st_as_sf(shp))
 
+#convert a simple features (back) to a spatVector using vect()
+(spat_vector<-vect(simple_features))
+#seem familiar?
 
-#####Raster package
-#create raster - define columns, rows, and crs 
-r <- raster(ncol=13, nrow=10,crs=crs(shp))
+#####Raster 
 
-#assign values to raster
-values(r) <- c(rep(0,16),1,rep(0,5),1,rep(0,7),1,0,0,
-               0,1,rep(0,7),rep(1,7),0,0,0,0,0,1,1,0,1,1,1,0,1,1,0,0,0,rep(1,11),
-               0,0,1,0,rep(1,7),0,1,0,0,1,0,1,rep(0,5),1,0,1,0,0,0,0,0,1,1,0,
-               1,1,rep(0,17))
+# Create a matrix of values using the terra package
+
+?rast #see what arguments are required to make a raster note :: calls package terra
+
+#let's copy our crs from 'shp'
+crs.shp<-crs(shp)
+
+#let's inspect
+crs.shp
+
+#okay lets make our raster from scratch & give it 10 rows and 13 columns 
+#plotted in WGS84
+r <- rast(nrows=10, ncols=13, crs=crs.shp)
+
+#create 10 x 13 =130 values and assign values to raster
+values(r)<- c(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,1,
+              0,0,0,0,0,0,0,1,1,1,1,1,1,1,0,0,0,0,0,1,1,0,1,1,1,0,1,1,0,0,0,1,1,1,1,
+              1,1,1,1,1,1,1,0,0,1,0,1,1,1,1,1,1,1,0,1,0,0,1,0,1,0,0,0,0,0,1,0,1,0,0,
+              0,0,0,1,1,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0)
+
 #inspect raster
 r
+
+#look at its crs
+crs(r,describe=T)
 
 #plot raster
 plot(r,col=c("black","green"))
@@ -160,7 +131,7 @@ plot(r,col=c("black","green"))
 writeRaster(r,"myraster.tif",overwrite=T)
 
 #load raster from file
-r2 <- raster("myraster.tif")
+r2 <- rast("myraster.tif")
 
 #plot raster to inspect
 plot(r2,col=c("black","purple"))

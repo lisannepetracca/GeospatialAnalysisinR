0073ecf7dfaa524439706bfb8da41e9dc31f2a1c
Matt Smith <mmsecologyres@gmail.com>
2025-05-14 11:47:00 -0500

    added landscapemetrics, spatstat, and gstat packages

--- PreWorkshop_Steps.R
+++ PreWorkshop_Steps.R
@@ -19,7 +19,7 @@ setwd("C:/PASTE THE LOCATION OF WHERE YOUR Geospatial_Analysis_in_R FOLDER IS")
 #*IF IT ASKS FOR YOUR CRAN MIRROR YOU CAN SAY CLOUD
 install.packages(c("terra", "ggplot2", "rnaturalearth", "tidyterra", "viridis", "mapview",
                    "ggspatial", "cowplot", "sf", "tmap", "rgbif","keyring","ggmap","move2",
-                   "adehabitatHR","FedData","raster","tidyverse"),dependencies=T)
+                   "adehabitatHR","FedData","raster","tidyverse","landscapemetrics","spatstat","gstat"),dependencies=T)
 
 #*RED TEXT DOES NOT MEAN IT DIDN'T WORK. THERE IS ONLY AN ISSUE IF YOU SEE SOMETHING LIKE "Error in install.packages>"***
 
@@ -42,7 +42,9 @@ library(adehabitatHR)
 library(FedData)
 library(raster)
 library(mapview)
-
+library(landscapemetrics)
+library(spatstat)
+library(gstat)
 #we are ALL GOOD on packages if you do not get any error messages after running these "library" lines
 
 #and then read in two types of geospatial data

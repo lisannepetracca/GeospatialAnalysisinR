cfb6d1a4e0bdb68e92bddb67535c8d179157b5b2
cmnouzille <cmnouzille@gmail.com>
2025-06-08 14:42:24 -0500

    changes to first optional exercise

--- EXERCISE_LoadingData.R
+++ EXERCISE_LoadingData.R
@@ -4,17 +4,23 @@ setwd("YOUR WORKING DIRECTORY HERE")
 #Example: setwd("C:/Users/KUJNH012/Documents/OcelotPhd/Rcode/GeospatialAnalysisinR")
 
 #load in a .tif image
-ras <- rast("moon.tif")
+ras <- rast("TorresDelPaine.tif")
 plot(ras)
 
 #inspect ras to get the min and max x and y values
-ras
-x<-runif(25,1,358) #this is generating 25 random values between 1 and 358
-y<-runif(25,1,500) #this is generating 25 random values between 1 and 500
-df<-data.frame(x=x,y=y)
-points<-vect(df,geom=c("x","y"), crs=crs(ras))
+ext <- ext(ras)
+ext
 
-plot(ras,add=F)
+#create x coordinates
+x <- seq(ext[1], ext[2], length.out = 25)
+
+# create y coordinares
+y <- seq(ext[3], ext[4], length = 25)
+
+#create grid of x and y points
+grid <- expand.grid(x = x, y = y)
+
+# convert to SpatVector
+points<-vect(grid,geom=c("x","y"), crs=crs(ras))
+plot(ras, add = F)
 plot(points,col="red",pch=16,add=T)
-
-
